bc51248bff64ae6bc844e372f927f0b60c8c851d
Chris Tralie <chris.tralie@gmail.com>
2022-04-19 16:59:29 -0400

    Refactoring annotation loader

---
+++ annotations.js
@@ -0,0 +1,35 @@
+
+let allAnnotations = [];
+let annotationPromise = new Promise((resolve, reject) => {
+    $.getJSON( "allAnnotations.json", function(data) {
+        // Step 1: Filter out spurious annotations
+        for (let i = 0; i < data.length; i++) {
+            // First save text just in case the last one is filtered out
+            let anno = {};
+            anno.text = data[i][data[i].length-1].text;
+            allAnnotations.push(anno);
+            data[i] = data[i].filter(elem => 
+                elem.x < 10 && elem.y < 10 && elem.z < 10
+            );
+        }
+        // Step 2: Compute centroids and add all annotations to the model
+        for (let i = 0; i < data.length; i++) {
+            let x = 0;
+            let y = 0;
+            let z = 0;
+            for (let j = 0; j < data[i].length; j++) {
+                x += data[i][j].x;
+                y += data[i][j].y;
+                z += data[i][j].z;
+            }
+            x /= data[i].length;
+            y /= data[i].length;
+            z /= data[i].length;
+            let anno = allAnnotations[i];
+            anno.x = x;
+            anno.y = y;
+            anno.z = z;
+        }
+        resolve();
+    });
+});
--- main.js
+++ main.js
@@ -5,37 +5,7 @@ let matname = 'models/Final/medres.mtl';
 let canvas = new ArtCanvas({zpos: 3, 
                             pickingNew: false});
 canvas.loadMesh(filename, matname);
-
-
-let allAnnotations = [];
-$.getJSON( "allAnnotations.json", function(data) {
-    // Step 1: Filter out spurious annotations
-    for (let i = 0; i < data.length; i++) {
-        // First save text just in case the last one is filtered out
-        let anno = {};
-        anno.text = data[i][data[i].length-1].text;
-        allAnnotations.push(anno);
-        data[i] = data[i].filter(elem => 
-            elem.x < 10 && elem.y < 10 && elem.z < 10
-        );
-    }
-    // Step 2: Compute centroids and add all annotations to the model
-    for (let i = 0; i < data.length; i++) {
-        let x = 0;
-        let y = 0;
-        let z = 0;
-        for (let j = 0; j < data[i].length; j++) {
-            x += data[i][j].x;
-            y += data[i][j].y;
-            z += data[i][j].z;
-        }
-        x /= data[i].length;
-        y /= data[i].length;
-        z /= data[i].length;
-        let anno = allAnnotations[i];
-        anno.x = x;
-        anno.y = y;
-        anno.z = z;
-    }
+annotationPromise.then(function() {
     canvas.loadAnnotations(allAnnotations);
 });
+
